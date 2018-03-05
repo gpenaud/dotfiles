d76a5da92c3f3eb2517f6d913073df0726d35e32
Guillaume Penaud <gpenaud@xilopix.com>
2018-03-05 11:07:52 +0100

    [bashrc] - Find a way to avoid in-file gitignore lines

--- setup.sh
+++ setup.sh
@@ -1,9 +1,21 @@
 #! /bin/bash
 
-export DOTFILES_DIR=${DOTFILES_DIR:-$HOME}/dotfiles
+export DOTFILES_DIR=${DOTFILES_DIR:-$HOME/dotfiles}
+export DOTFILES_ENV=${DOTFILES_DIR}/environment/${DOTFILES_ENV}
 
-rm $HOME/.bashrc
-ln -s $DOTFILES_DIR/bash/bashrc $HOME/.bashrc
+rm ${HOME}/.bashrc
+ln -s ${DOTFILES_DIR}/bash/bashrc ${HOME}/.bashrc
 
-sed -i "s@__DOTFILES_DIR__@$DOTFILES_DIR@" $DOTFILES_DIR/bash/bashrc
-sed -i "s@__DOTFILES_ENV__@$DOTFILES_ENV@" $DOTFILES_DIR/bash/bashrc
+# set env file content
+cat << EOF > ${DOTFILES_DIR}/bash/env_loader
+#!/usr/bin/env bash
+export DOTFILES_PATH=${DOTFILES_DIR}
+export DOTFILES_ENV=${DOTFILES_ENV}
+
+if [ -f "${DOTFILES_ENV}" ]; then
+  source ${DOTFILES_ENV}
+else
+  echo "${DOTFILES_ENV} doest not exists"
+  exit 1
+fi
+EOF
